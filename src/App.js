141740942fac7683de4e22640a8482a1ef5131a2
VectorSophie <jay7math@gmail.com>
2025-10-30 11:23:58 +0900

    feat(App): initial app setting

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
-  async run() {}
+  async run() {
+    try{
+      const purchaseAmount = await Console.readLineAsync('구입금액을 입력해 주세요.\n');
+    } catch(error) {
+      Console.print(error.message);
+      throw error;
+    }
+  }
 }
 
 export default App;
