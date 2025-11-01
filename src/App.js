4fa07a7ffc6f2b2ffbdb54b78bd1df54b9752301
VectorSophie <jay7math@gmail.com>
2025-11-02 03:02:28 +0900

    refactor(App): actually call upon implemented feature

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Console } from '@woowacourse/mission-utils';
+import LottoGame from './LottoGame.js';
 
 class App {
   async run() {
-    try{
-      const purchaseAmount = await Console.readLineAsync('구입금액을 입력해 주세요.\n');
-    } catch(error) {
+    try {
+      const game = new LottoGame();
+      await game.start();
+    } catch (error) {
       Console.print(error.message);
       throw error;
     }
