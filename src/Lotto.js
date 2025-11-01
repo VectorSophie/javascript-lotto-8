fc08491eab9d03b3bcf76592a6c26c2ffab47b05
VectorSophie <jay7math@gmail.com>
2025-11-02 02:48:00 +0900

    feat(Lotto): validation logic

--- src/Lotto.js
+++ src/Lotto.js
@@ -3,16 +3,20 @@ class Lotto {
 
   constructor(numbers) {
     this.#validate(numbers);
-    this.#numbers = numbers;
+    this.#numbers = [...numbers].sort((a, b) => a - b);
   }
 
   #validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-    }
+    if (numbers.length !== 6) throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+    const invalid = numbers.some(num => num < 1 || num > 45); // range setting
+    if (invalid) throw new Error('[ERROR] 로또 번호는 1부터 45 사이여야 합니다.');
+    const unique = new Set(numbers); // using set for doubles
+    if (unique.size !== 6) throw new Error('[ERROR] 로또 번호에 중복이 있습니다.');
   }
 
-  // TODO: 추가 기능 구현
+  getNumbers() {
+    return this.#numbers;
+  }
 }
 
 export default Lotto;
